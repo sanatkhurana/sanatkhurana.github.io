ace73ae464e6b2a613df89fe5691d6914b73565a
sanatkhurana <sanatkhurana@gmail.com>
2024-12-24 15:40:57 +0530

    Add personal site files

---
+++ typewriter.js
@@ -0,0 +1,53 @@
+/* Based on: https://css-tricks.com/snippets/css/typewriter-effect */
+
+var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = this.txt;
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+    }
+
+    setTimeout(function() {
+    that.tick();
+    }, delta);
+};
+
+function initTypewriter() {
+    var elements = document.getElementsByClassName('typewriter');
+    for (var i = 0; i < elements.length; i++) {
+        var toRotate = elements[i].getAttribute('twlist');
+        var period = 2000;
+        if (toRotate) {
+          new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+};
